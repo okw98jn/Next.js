7e7bfcbca9a57b2744511cdb737a5b596f9f1502
okawa <0cu2045d@gmail.com>
2023-12-07 00:08:15 +0900

    編集用モーダル作成

--- frontend/app/admin/admin/edit/EditForm.tsx
+++ frontend/app/admin/admin/edit/EditForm.tsx
@@ -1,14 +1,51 @@
+"use client"
+
 import React, { FC, memo } from 'react'
+import { FormProvider, useForm } from 'react-hook-form';
+import InputControl from '../../components/form/InputControl';
+import SelectBox from '../../components/form/SelectBox';
+import Radio from '../../components/form/Radio';
+import { Button } from '@/components/ui/button';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { AdminRoleList, AdminStatus, AdminStatusList } from '../../const/AdminConst';
+import { AdminNewType } from '../types/AdminType';
+import UpdateValidator from './UpdateValidator';
 
 type Props = {
     id: number;
+    setEditModalOpen?: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const EditForm: FC<Props> = memo(({ id }) => {
+const EditForm: FC<Props> = memo(({ id, setEditModalOpen }) => {
+    const form = useForm<AdminNewType>({
+        resolver: zodResolver(UpdateValidator),
+        defaultValues: {
+            name: "",
+            login_id: "",
+            role: "",
+            status: AdminStatus.StatusValid,
+        },
+    })
+
+    function onSubmit(values: AdminNewType) {
+        console.log(values)
+        setEditModalOpen && setEditModalOpen(false);
+    }
+
     return (
-        <span>
-            {id}
-        </span>
+        <FormProvider {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/2">
+                <div className="space-y-3">
+                    <InputControl name='name' label='名前' placeholder='山田太郎' />
+                    <InputControl name='login_id' label='ログインID' placeholder='ログインID' />
+                    <SelectBox name='role' label='権限' placeholder='権限を選択してください' selectItems={AdminRoleList} />
+                    <Radio name='status' label='ステータス' radioItems={AdminStatusList} />
+                </div>
+                <div className='mt-6'>
+                    <Button type="submit">登録</Button>
+                </div>
+            </form>
+        </FormProvider>
     )
 })
 
--- frontend/components/modal/EditModal.tsx
+++ frontend/components/modal/EditModal.tsx
@@ -15,15 +15,21 @@ type Props = {
 }
 
 const EditModal: FC<Props> = memo(({ editModalOpen, setEditModalOpen, modalText, editForm }) => {
+    const dialogTitle = `${modalText}編集`;
+    const dialogDescription = `${modalText}を編集することができます`;
+
     return (
         <Dialog open={editModalOpen} onOpenChange={setEditModalOpen}>
-            <DialogContent>
+            <DialogContent className='p-10'>
                 <DialogHeader>
-                    <DialogTitle>{modalText}</DialogTitle>
+                    <DialogTitle className='mb-2'>{dialogTitle}</DialogTitle>
                     <DialogDescription>
-                        {editForm}
+                        <span className='block'>{dialogDescription}</span>
+                        <span className='block'>編集したい項目を入力してください</span>
                     </DialogDescription>
                 </DialogHeader>
+                {/* 各ドメインに対応 */}
+                {React.cloneElement(editForm as React.ReactElement, { setEditModalOpen })}
             </DialogContent>
         </Dialog>
     )
