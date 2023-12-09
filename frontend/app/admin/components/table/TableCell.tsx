2f6b717d8470d52d852dbb333b6df59cdaed10c9
okawa jin <0cu2045d@gmail.com>
2023-12-09 13:15:41 +0900

    テーブルセルの高さ調整

--- frontend/app/admin/components/table/TableCell.tsx
+++ frontend/app/admin/components/table/TableCell.tsx
@@ -7,7 +7,7 @@ type TableCellProps = {
 const TableCell: FC<TableCellProps> = memo(({ text }) => {
     return (
         <td className="whitespace-nowrap">
-            <div className="px-6 py-4">
+            <div className="px-6 py-2.5">
                 <div className="flex items-center gap-x-3">
                     <span className="block text-sm text-gray-500">{text}</span>
                 </div>
