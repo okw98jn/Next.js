15fe48c022b5db1a5e678bbc8f8eec4616a2abc7
okawa jin <0cu2045d@gmail.com>
2023-12-16 17:25:15 +0900

    動作が遅いためホットリロード無効化

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    webpackDevMiddleware: config => {
-        config.watchOptions = {
-            poll: 800,
-            aggregateTimeout: 300,
-        }
-        return config
-    },
 }
 
 module.exports = nextConfig
